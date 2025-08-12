37507b7a5c08987ddc22f8013fd2f97988b9918a
yogi2023-cne <yogik@cnetric.com>
2025-08-12 17:46:53 +0000

    chore(vendure): add vendure from scratch

--- packages/pieces/community/vendure/src/index.ts
+++ packages/pieces/community/vendure/src/index.ts
@@ -1,26 +1,13 @@
 
     import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-import { getAllProducts } from "./lib/actions/get-all-products";
-import { getAllCategories } from "./lib/actions/get-all-category";
-import { customerLogin} from "./lib/actions/customer-login";
-import { adminLogin } from "./lib/actions/superadmin-login";
-import { getCustomerCart } from "./lib/actions/get-customer-cart";
-import { addItemToOrder } from "./lib/actions/add-item-to-cart";
-import { adjustOrderLine } from "./lib/actions/adjust-order-line";
-import { registerCustomerAccount } from "./lib/actions/customer-signup";
-import { getAllOrders } from "./lib/actions/get-customer-orders";
-import { getOrderById } from "./lib/actions/order-details";
-import { removeOrderLine } from "./lib/actions/remove-order-line";
-import { getCustomerDetails } from "./lib/actions/get-user-details";
-import { checkout } from "./lib/actions/checkout";
-
+    import { getAllProducts } from "./lib/actions/get-all-products";
     export const vendure = createPiece({
       displayName: "Vendure",
       auth: PieceAuth.None(),
       minimumSupportedRelease: '0.36.1',
-      logoUrl: "http://4.240.112.193:5256/admin/assets/logo-login.webp",
+      logoUrl: "https://cdn.activepieces.com/pieces/vendure.png",
       authors: [],
-      actions: [getAllProducts, getAllCategories, customerLogin, adminLogin, getCustomerCart, addItemToOrder,adjustOrderLine,registerCustomerAccount,getAllOrders,getOrderById,removeOrderLine,getCustomerDetails,checkout],
+      actions: [getAllProducts],
       triggers: [],
     });
     
--- packages/pieces/community/vendure/src/lib/actions/get-all-products.ts
+++ packages/pieces/community/vendure/src/lib/actions/get-all-products.ts
@@ -50,3 +50,4 @@ export const getAllProducts = createAction({
 //   triggers: [],
 //   authors: [],
 // });
+
